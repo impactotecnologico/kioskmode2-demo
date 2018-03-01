af70d6e55321c52a8b8bb0434882242f5fe95f1c
Impacto Tecnologico <josejulian@impactotecnologico.net>
2018-03-01 12:39:34 +0100

    version estable de demo de KioskMode2 para Android

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,59 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Platform } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
+import {WindowRef} from '../../app/WindowRef';
+
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  public exitCounter:number = 1;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private platform: Platform, private winRef: WindowRef, public alertCtrl: AlertController) {
+      platform.registerBackButtonAction(() => {
+        if (this.exitCounter==7) {
+          this.presentConfirm();  
+        } else {
+            this.exitCounter++;
+        }
+      }, 0)
+  }
 
+  showApps() {
+      this.winRef.nativeWindow.plugins.KPlugin.showApps();
+  }
+
+  uninstall() {
+      this.winRef.nativeWindow.plugins.KPlugin.uninstall();
+  }
+
+   presentConfirm() {
+    let alert = this.alertCtrl.create({
+      title: 'Confirm Exit',
+      message: 'Do you want Exit?',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+            this.exitCounter = 1;
+          }
+        },
+        {
+          text: 'Yes',
+          handler: () => {
+            console.log('Yes clicked');
+            this.exitCounter = 1;
+            this.platform.exitApp();
+          }
+        }
+      ]
+    });
+     alert.present();
   }
 
 }
